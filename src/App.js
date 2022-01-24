f7c02fa99165b031c47b679f5eedb6bddda78a71
salrey <tekdehn@gmail.com>
2022-01-24 15:49:54 -0500

    Set up app, dependencies, public docs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// DEPENDENCIES
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+// COMPONENTS
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
+import New from "./Pages/New";
+import Reload from "./Pages/Reload";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={ <Home />} />
+          <Route path="/transactions" element={ <Index />} />
+          <Route path="/transactions/new" element={ <New />} />
+          <Route path="/transactions/:index" element={ <Show />} />
+          <Route path="/transactions/:index/edit" element={ <Edit />} />
+          <Route path="*" element={ <Reload />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
